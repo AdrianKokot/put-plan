9e3f1030303e7c8b44d697e46ed9ca9eab6aa14b
Adrian Kokot <splateusz@gmail.com>
2021-02-23 19:10:30 +0100

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'put-plan';
+  isEvenWeek: boolean;
+  weekTypeCorrect = false;
+
+  constructor() {
+    const now = new Date();
+    const date = new Date(now.getFullYear(), 0, 1);
+    this.isEvenWeek = (Math.ceil(((new Date(now.getFullYear(), now.getMonth(), now.getDate() - date.getDate()).getTime() / 86400000) + date.getDay() + 1) / 7) % 2 == 0) === this.weekTypeCorrect;
+  }
+
+  changeWeek($e: { preventDefault: () => void; } | null = null) {
+    if ($e != null) {
+      $e.preventDefault();
+    }
+    this.isEvenWeek = !this.isEvenWeek;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { TimetableComponent } from './components/timetable/timetable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
