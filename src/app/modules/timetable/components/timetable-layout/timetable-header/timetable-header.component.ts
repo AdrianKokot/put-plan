b450f9f091ccdb0284c1264fa5070891748bd437
Adrian Kokot <adrian.kokot@bbraun.com>
2021-09-24 14:49:18 +0200

    Add timetable header following current weekday

--- src/app/modules/timetable/components/timetable-layout/timetable-header/timetable-header.component.ts
+++ src/app/modules/timetable/components/timetable-layout/timetable-header/timetable-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 import { Timetable } from "../../../timetable";
 
 @Component({
@@ -6,7 +6,7 @@ import { Timetable } from "../../../timetable";
   templateUrl: './timetable-header.component.html',
   styles: []
 })
-export class TimetableHeaderComponent {
+export class TimetableHeaderComponent implements AfterViewInit {
   private _selectedWeekDayIndex!: number;
 
   public get selectedWeekDayIndex(): number {
@@ -16,17 +16,32 @@ export class TimetableHeaderComponent {
   @Input()
   public set selectedWeekDayIndex(value: number) {
     this._selectedWeekDayIndex = value;
+    this.updateContainerPosition();
   }
 
   @Output() public selectedWeekDayIndexChange = new EventEmitter<number>();
 
-  @ViewChild('timetableWeekDaysContainer') timetableWeekDaysContainer!: ElementRef;
-
   public weekDays = Timetable.WeekDays;
+
+  private buttons: NodeListOf<HTMLButtonElement> | null = null;
+  private componentElement: HTMLElement;
+
+  constructor(element: ElementRef) {
+    this.componentElement = element.nativeElement;
+  }
+
+  ngAfterViewInit(): void {
+    this.buttons = this.componentElement.querySelectorAll('button');
+    this.updateContainerPosition();
+  }
 
   public selectWeekDayIndex(index: number): void {
     if (window.innerWidth < 768) {
       this.selectedWeekDayIndexChange.emit(index);
     }
+  }
+
+  private updateContainerPosition(): void {
+    this.buttons && this.buttons.item(this.selectedWeekDayIndex).scrollIntoView({ block: 'nearest', behavior: 'smooth' });
   }
 }
