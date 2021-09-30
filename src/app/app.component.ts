6ba94e44183b12a484072ba2f8994233fc03e6c4
Adrian Kokot <adrian.kokot@bbraun.com>
2021-09-30 15:10:14 +0200

    Remove //@ts comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { firebaseConfig } from 'src/environments/firebase';
+
+declare global {
+  interface Window {
+    dataLayer: (string | Date | (string | Date)[])[];
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -8,19 +15,26 @@ import { firebaseConfig } from 'src/environments/firebase';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
+
+    if (environment.production) {
+      this.addGTag(firebaseConfig.measurementId);
+    }
+
+  }
+
+  private addGTag(gtagId: string): void {
     const gTag = document.createElement('script');
     gTag.type = 'text/javascript';
     gTag.async = true;
-    gTag.src = `https://www.googletagmanager.com/gtag/js?id=${firebaseConfig.measurementId}`;
+    gTag.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
     gTag.onload = function () {
-      // @ts-ignore
+
       window.dataLayer = window.dataLayer || [];
-      // @ts-ignore
-      function gtag() { window.dataLayer.push(arguments); }
-      // @ts-ignore
+
+      function gtag(...args: (string | Date)[]) { window.dataLayer.push(args); }
+
       gtag('js', new Date());
-      // @ts-ignore
-      gtag('config', firebaseConfig.measurementId);
+      gtag('config', gtagId);
 
     }
     document.body.appendChild(gTag);
