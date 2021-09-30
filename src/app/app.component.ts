df720fbd9deb5629e86e73f4b4988ca2c4dab8e9
Adrian Kokot <adrian.kokot@bbraun.com>
2021-09-30 14:37:42 +0200

    Remove angular/fire package to optimize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { firebaseConfig } from 'src/environments/firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const gTag = document.createElement('script');
+    gTag.type = 'text/javascript';
+    gTag.async = true;
+    gTag.src = `https://www.googletagmanager.com/gtag/js?id=${firebaseConfig.measurementId}`;
+    gTag.onload = function () {
+      // @ts-ignore
+      window.dataLayer = window.dataLayer || [];
+      // @ts-ignore
+      function gtag() { window.dataLayer.push(arguments); }
+      // @ts-ignore
+      gtag('js', new Date());
+      // @ts-ignore
+      gtag('config', firebaseConfig.measurementId);
+
+    }
+    document.body.appendChild(gTag);
+  }
+
 }
--- src/environments/firebase.ts
+++ src/environments/firebase.ts
@@ -1,11 +1,10 @@
-
 export const firebaseConfig = {
   apiKey: 'AIzaSyD719IBJ41xZkhaQbAJy2GREdDurAnbR8c',
-  authDomain: 'put-plan.firebaseapp.com',
+  // authDomain: 'put-plan.firebaseapp.com',
   databaseURL: 'https://put-plan-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'put-plan',
-  storageBucket: 'put-plan.appspot.com',
-  messagingSenderId: '664689934224',
+  // storageBucket: 'put-plan.appspot.com',
+  // messagingSenderId: '664689934224',
   appId: '1:664689934224:web:0478862378fa306f80bd15',
-  measurementId: 'G-LJ1LVS0CR5'
+  measurementId: 'G-LJ1LVS0CR5',
 }
